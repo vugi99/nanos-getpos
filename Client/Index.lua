fd60d5a97b790f34318559a185a79ed527dc54a0
vugi99 <43786647+vugi99@users.noreply.github.com>
2021-10-27 13:04:59 +0200

    v0.1.0

---
+++ Client/Index.lua
@@ -0,0 +1,35 @@
+
+
+local POS_TEXT = Render.AddText(3451, "", Vector2D(math.floor(Render.GetViewportSize().X * 0.5), math.floor(Render.GetViewportSize().Y * 0.95)), 0, 12, Color.WHITE, 0, true, true, false, Vector2D(), Color.WHITE, false,  Color.WHITE)
+
+
+Client.Subscribe("Tick", function(ds)
+    local ply = Client.GetLocalPlayer()
+    if ply then
+        local char = ply:GetControlledCharacter()
+        if char then
+            local loc = char:GetLocation()
+            local rot = char:GetRotation()
+
+            Render.UpdateItemText(3451, POS_TEXT, tostring(loc) .. ", YAW : " .. tostring(rot.Yaw))
+        end
+    end
+end)
+
+Client.Subscribe("Console", function(text)
+    if text == "/cl" then
+        local ply = Client.GetLocalPlayer()
+        if ply then
+            local char = ply:GetControlledCharacter()
+            if char then
+                local loc = char:GetLocation()
+                local rot = char:GetRotation()
+
+                print("CHARACTER LOCATION AND YAW")
+                print("Vector(" .. tostring(loc.X) .. ", " .. tostring(loc.Y) .. ", " .. tostring(loc.Z) .. ")")
+                print(tostring(rot.Yaw))
+                print("__________________________________________________________________________")
+            end
+        end
+    end
+end)
