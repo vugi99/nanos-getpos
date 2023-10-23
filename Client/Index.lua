095a88445229ffdae890459f0bdd517fd8110b5f
vugi99 <voltaism.mail@gmail.com>
2023-10-23 16:00:01 +0200

    v0.2.3

--- Client/Index.lua
+++ Client/Index.lua
@@ -1,4 +1,24 @@
 
+local function getlocandrot()
+    local loc
+    local rot
+
+    local ply = Client.GetLocalPlayer()
+    if ply then
+        local char = ply:GetControlledCharacter()
+        if char then
+            loc = char:GetLocation()
+            rot = char:GetRotation()
+        else
+            loc = ply:GetCameraLocation()
+            rot = ply:GetCameraRotation()
+        end
+    end
+
+    return loc, rot
+end
+
+
 local canvas = Canvas(
     true,
     Color(0, 0, 0, 0),
@@ -6,32 +26,20 @@ local canvas = Canvas(
     true
 )
 canvas:Subscribe("Update", function(self, width, height)
-    local ply = Client.GetLocalPlayer()
-    if ply then
-        local char = ply:GetControlledCharacter()
-        if char then
-            local loc = char:GetLocation()
-            local rot = char:GetRotation()
-
-            self:DrawText(tostring(loc) .. ", YAW : " .. tostring(rot.Yaw), Vector2D(math.floor(Client.GetViewportSize().X * 0.5), math.floor(Client.GetViewportSize().Y * 0.95)), 0, 12, Color.WHITE, 0, true, true, Color(0, 0, 0, 0), Vector2D(), false, Color.WHITE)
-        end
+    local loc, rot = getlocandrot()
+    if (loc and rot) then
+        self:DrawText(tostring(loc) .. ", YAW : " .. tostring(rot.Yaw), Vector2D(math.floor(Viewport.GetViewportSize().X * 0.5), math.floor(Viewport.GetViewportSize().Y * 0.95)), 0, 12, Color.WHITE, 0, true, true, Color(0, 0, 0, 0), Vector2D(), false, Color.WHITE)
     end
 end)
 
-Client.Subscribe("Console", function(text)
+Console.Subscribe("PlayerSubmit", function(text)
     if text == "/cl" then
-        local ply = Client.GetLocalPlayer()
-        if ply then
-            local char = ply:GetControlledCharacter()
-            if char then
-                local loc = char:GetLocation()
-                local rot = char:GetRotation()
-
-                print("CHARACTER LOCATION AND YAW")
-                print("Vector(" .. tostring(loc.X) .. ", " .. tostring(loc.Y) .. ", " .. tostring(loc.Z) .. ")")
-                print(tostring(rot.Yaw))
-                print("__________________________________________________________________________")
-            end
+        local loc, rot = getlocandrot()
+        if (loc and rot) then
+            print("CHARACTER/CAMERA LOCATION AND YAW")
+            print("Vector(" .. tostring(loc.X) .. ", " .. tostring(loc.Y) .. ", " .. tostring(loc.Z) .. ")")
+            print(tostring(rot.Yaw))
+            print("__________________________________________________________________________")
         end
     end
 end)
